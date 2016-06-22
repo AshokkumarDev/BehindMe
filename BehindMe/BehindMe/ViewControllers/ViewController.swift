23bc514387574965eff7aa5595fc97801a4eb619
AshokkumarDev <ashokkumar89.dev@gmail.com>
2016-06-22 12:39:19 +0530

    File Structuring
    Initial commit with basic file structuring

---
+++ BehindMe/BehindMe/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BehindMe
+//
+//  Created by Ashok Kumar on 6/22/16.
+//  Copyright © 2016 AshokKumarDeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
