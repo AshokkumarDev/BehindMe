7c2f591243913045373aac96972f9ce502f7fcef
AshokkumarDev <ashokkumar89.dev@gmail.com>
2016-06-24 14:18:50 +0530

    Basic functionalities added
    Search and flickr fetch logic added

---
+++ BehindMe/BehindMe/ViewControllers/FlickrPhotosViewController.swift
@@ -0,0 +1,108 @@
+//
+//  FlickrPhotosViewController.swift
+//  BehindMe
+//
+//  Created by Ashok Kumar on 6/24/16.
+//  Copyright © 2016 AshokKumarDeva. All rights reserved.
+//
+
+import UIKit
+
+
+
+class FlickrPhotosViewController: UICollectionViewController {
+
+    
+    private let reuseIdentifier = "FlickrCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    private var searches = [FlickrSearchResults]()
+    private let flickr = Flickr()
+    
+    func photoForIndexPath(indexPath: NSIndexPath) -> FlickrPhoto {
+        return searches[indexPath.section].searchResults[indexPath.row]
+    }
+    
+    
+}
+
+extension FlickrPhotosViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // 1
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+        textField.addSubview(activityIndicator)
+        activityIndicator.frame = textField.bounds
+        activityIndicator.startAnimating()
+        flickr.searchFlickrForTerm(textField.text!) {
+            results, error in
+            
+            //2
+            activityIndicator.removeFromSuperview()
+            if error != nil {
+                print("Error searching : \(error)")
+            }
+            
+            if results != nil {
+                //3
+                print("Found \(results!.searchResults.count) matching \(results!.searchTerm)")
+                self.searches.insert(results!, atIndex: 0)
+                
+                //4
+                self.collectionView?.reloadData()
+            }
+        }
+        
+        textField.text = nil
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension FlickrPhotosViewController : UICollectionViewDelegateFlowLayout {
+    //1
+    func collectionView(collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                               sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        let flickrPhoto =  photoForIndexPath(indexPath)
+        //2
+        if var size = flickrPhoto.thumbnail?.size {
+            size.width += 10
+            size.height += 10
+            return size
+        }
+        return CGSize(width: 100, height: 100)
+    }
+    
+    //3
+    func collectionView(collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                               insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+}
+
+extension FlickrPhotosViewController  {
+    
+    //1
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return searches.count
+    }
+    
+    //2
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return searches[section].searchResults.count
+    }
+    
+    //3
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! FlickrPhotoCell
+        //2
+        let flickrPhoto = photoForIndexPath(indexPath)
+        cell.backgroundColor = UIColor.blackColor()
+        //3
+        cell.photoImageView.image = flickrPhoto.thumbnail
+        
+        return cell
+    }
+}
---
+++ BehindMe/BehindMe/Views/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  BehindMe
+//
+//  Created by Ashok Kumar on 6/24/16.
+//  Copyright © 2016 AshokKumarDeva. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet var photoImageView: UIImageView!
+}
